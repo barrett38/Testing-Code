f8d09a395644fe6939916a0a03c79aa4024e5d1c
Barrett Anderson <barrett1614@gmail.com>
2025-02-03 11:18:16 -0600

    Changed number of commits

--- Commit.js
+++ Commit.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const simpleGit = require("simple-git");
 const git = simpleGit();
 
-const min = 6;
-const max = 8;
+const min = 15;
+const max = 20;
 const date = new Date();
 const maxCommit = Math.floor(Math.random() * (max - min + 1)) + min;
 
